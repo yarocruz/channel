baab9512123a59c372655b3d2441a4f9d91dd942
yarocruz <yarocruz@gmail.com>
2020-07-28 15:26:57 -0400

    made basic Navbar component and styled it

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import './App.css';
+import Navbar from "./components/Navbar";
 
 function App() {
   return (
     <div className="App">
-      <h1>Channel - RSS Reader</h1>
+      <Navbar />
     </div>
   );
 }
---
+++ src/components/Navbar.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+export default function Navbar() {
+    return (
+        <div className='navbar'>
+            <h1 className="logo">Channel</h1>
+            <nav>
+                <a href="https://en.wikipedia.org/wiki/RSS">What is RSS?</a>
+                <a href="#">Guide</a>
+            </nav>
+        </div>
+    )
+}
