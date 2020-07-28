9b716be1929e26f98081be184a8c5ea0dfc61c7b
yarocruz <yarocruz@gmail.com>
2020-07-28 17:39:35 -0400

    starting adding state to the addFeed form

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -1,14 +1,29 @@
-import React from "react";
+import React, { useState } from "react";
 
 export default function Sidebar() {
+    const [feeds, setFeeds] = useState([]);
+    const [feedName, setFeedName ] = useState('')
+
+    const addFeed = (e) => {
+        e.preventDefault();
+        setFeeds([...feeds, feedName])
+        console.log(feeds);
+        setFeedName('');
+    }
+
     return (
         <div className="sidebar">
             <ul className="sidebar--feed-list">
-                <li>Placeholder Feed</li>
+                {feeds.length ?
+                    feeds.map((feed, i) => (
+                        <li key={i}>{feed}</li>
+                    )) :
+                    <li>No feeds yet.</li>
+                }
             </ul>
 
-            <form className="sidebar-form">
-                <input type="text"/>
+            <form className="sidebar-form" onSubmit={addFeed} >
+                <input type="text" value={feedName} onChange={e => setFeedName(e.target.value)}/>
                 <button>Add a Feed</button>
             </form>
         </div>
