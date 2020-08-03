9a25aba1d1976b2366db1cd042f58b416095539f
yarocruz <yarocruz@gmail.com>
2020-08-03 13:34:45 -0400

    brought trashicon component

--- src/components/FeedItem.js
+++ src/components/FeedItem.js
@@ -1,10 +1,12 @@
 import React from "react";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faTrash } from '@fortawesome/free-solid-svg-icons'
 
 export default function FeedItem({ title, onClick, onDelete }) {
     return (
-        <div>
-            <li onClick={onClick} className='sidebar--feed-title'>{title}</li>
-            <button onClick={onDelete}>Delete</button>
+        <div className='sidebar--feed-item'>
+            <li style={{ paddingRight: '15px'}} onClick={onClick} className='sidebar--feed-title'>{title}</li>
+            <FontAwesomeIcon className='sidebar--feed-delete' icon={faTrash} onClick={onDelete}/>
         </div>
     )
 }
