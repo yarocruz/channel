9bd435b8a175dcea90f42144be80d08dc423004c
yarocruz <yarocruz@gmail.com>
2020-08-04 16:51:22 -0400

    added the feed item count number to FeedItem component

--- src/components/FeedItem.js
+++ src/components/FeedItem.js
@@ -2,10 +2,12 @@ import React from "react";
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faTrash } from '@fortawesome/free-solid-svg-icons'
 
-export default function FeedItem({ title, onClick, onDelete }) {
+export default function FeedItem({ title, onClick, onDelete, feedCount }) {
     return (
         <div className='sidebar--feed-item'>
-            <li style={{ paddingRight: '15px'}} onClick={onClick} className='sidebar--feed-title'>{title}</li>
+            <li style={{ paddingRight: '15px'}} onClick={onClick} className='sidebar--feed-title'>
+                {title} <span className='sidebar--feed-count'>{feedCount}</span>
+            </li>
             <FontAwesomeIcon className='sidebar--feed-delete' icon={faTrash} onClick={onDelete}/>
         </div>
     )
