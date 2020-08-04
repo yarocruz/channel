fcdbe026226c915c03e1aadd4a11a543f5594beb
yarocruz <yarocruz@gmail.com>
2020-08-04 16:52:58 -0400

    pull in to conten area the feed title, description

--- src/components/FeedContent.js
+++ src/components/FeedContent.js
@@ -5,8 +5,12 @@ export default function FeedContent({ feeds }) {
     return (
         <div>
             <ul className='content-links'>
-                {feeds.length ?
-                    feeds.map(feed => {
+                <div className='content-links--info'>
+                    <h1>{feeds.title}</h1>
+                    <p>{feeds.description}</p>
+                </div>
+                {feeds.items.length ?
+                    feeds.items.map(feed => {
                         return feed.map(item => (
                             <li key={v4()} className='content-links--item'>
                                 <a  href={item.link}>{item.title}</a>
