4e9be95bbc14c3201ddcbb2f4ae11b6fcaf535cd
yarocruz <yarocruz@gmail.com>
2020-08-05 14:36:35 -0400

    added condition to truncate feed title string / title attribute to show on hover

--- src/components/FeedItem.js
+++ src/components/FeedItem.js
@@ -2,13 +2,13 @@ import React from "react";
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faTrash } from '@fortawesome/free-solid-svg-icons'
 
-export default function FeedItem({ title, onClick, onDelete, feedCount }) {
+export default function FeedItem({ title, onClick, onDelete }) {
     return (
         <div className='sidebar--feed-item'>
-            <li style={{ paddingRight: '15px'}} onClick={onClick} className='sidebar--feed-title'>
-                {title} <span className='sidebar--feed-count'>{feedCount}</span>
+            <li title={title} style={{ paddingRight: '15px'}} onClick={onClick} className='sidebar--feed-title'>
+                {title.length > 25 ? title.substring(0, 24) + '...' : title}
             </li>
-            <FontAwesomeIcon className='sidebar--feed-delete' icon={faTrash} onClick={onDelete}/>
+            <FontAwesomeIcon title='Delete Feed' className='sidebar--feed-delete' icon={faTrash} onClick={onDelete}/>
         </div>
     )
 }
