e35b60907f14aafb15f78b309696bc73d180a92d
yarocruz <yarocruz@gmail.com>
2020-08-06 17:21:29 -0400

    added routing to app

--- src/App.js
+++ src/App.js
@@ -2,13 +2,24 @@ import React from 'react';
 import './App.css';
 import Navbar from "./components/Navbar";
 import Sidebar from "./components/Sidebar";
+import {BrowserRouter as Router, Route, Switch} from "react-router-dom";
+import Guide from "./pages/Guide";
 
 function App() {
   return (
-    <div className="App">
-      <Navbar />
-      <Sidebar />
-    </div>
+    <Router>
+        <div className="App">
+            <Switch>
+                <Route exact path='/'>
+                    <Navbar />
+                    <Sidebar />
+                </Route>
+                <Route path='/guide'>
+                    <Guide />
+                </Route>
+            </Switch>
+        </div>
+    </Router>
   );
 }
 
