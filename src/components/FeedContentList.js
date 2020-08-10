03d49f5fcb04de4c93ba4c0525fa2494c75d5cb4
yarocruz <yarocruz@gmail.com>
2020-08-10 15:55:04 -0400

    adding published date to item links

--- src/components/FeedContentList.js
+++ src/components/FeedContentList.js
@@ -1,11 +1,11 @@
 import React from "react";
 
 export default function FeedContentList ({ item, onClick, isClicked }) {
-
+    let formatedDate = item.pubDate.replace(/\d\d:\d\d:\d\d\D+/, '');
     return (
         <>
             <li onClick={!isClicked ? onClick : null} className='content-links--item' >
-                <a href={item.link} target='_blank' rel='noopener noreferrer'>{item.title} </a>
+                <a href={item.link} target='_blank' rel='noopener noreferrer'>{item.title} <span className='content-links--item-date'>- {formatedDate}</span> </a>
             </li>
         </>
     )
