c7f1d0c30154b89a249e2ca09f69b8263c75ada1
yarocruz <yarocruz@gmail.com>
2020-08-12 16:50:45 -0400

    fixed some typos. made some edits

--- src/pages/Guide.js
+++ src/pages/Guide.js
@@ -17,17 +17,17 @@ export default function Guide() {
                         valid, you will get a browser alert (currently working on nicer solution) indicating that
                         the feed is not valid. A typical feed link could look like this <a href="https://whatthefuck.is/feed.xml">https://whatthefuck.is/feed.xml</a> Just make sure the link
                         is an actual rss feed address instead of the url of page. Most blogs have an
-                        RSS feed link and you can find the link in the navigation. Be aware however some sites bury them in
+                        RSS feed link somewhere in their navigation. Be aware however some sites bury them in
                         the footer somewhere. Some sites simply don't have an RSS feed.</p>
 
                     <h2>Deleting a Feed</h2>
                     <img style={{ maxWidth: '100%'}} src={DeleteFeed} alt="screenshot of input field"/>
-                    <p>To delete a feed simply hover the and click the x. Easy.</p>
+                    <p>To delete a feed simply hover and click the x.</p>
 
                     <h2>How this App is Working</h2>
-                    <p>This app is a work in progress. It's using the browser's localStorage to make the feeds you add persist.
+                    <p>This app is still a work in progress and at the moment it is best viewed on desktop sized screens. It's using the browser's localStorage to make the feeds you add persist.
                         I added the feature that keeps track of the items you read, but it's currently buggy. I'm hoping
-                        to keep adding features. It's built with React. If your a coder and want to collaborate or give me
+                        to keep adding features. It's built with React. If you're a coder and want to collaborate or give me
                         some pointers checkout the <a href="https://github.com/yarocruz/channel">code at Github</a> and let me know.</p>
                 </div>
             </div>
