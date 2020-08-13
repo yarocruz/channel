8f71801a9e171a442759dfc49c13ad4a7ee8855a
yarocruz <yarocruz@gmail.com>
2020-08-13 17:56:25 -0400

    Updates

---
+++ precache-manifest.5f90a5c61451d6f3215d1c7b63c6a9ed.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0e3597af8840cc83c0ba4d6305a2ba6a",
+    "url": "/channel/index.html"
+  },
+  {
+    "revision": "dccb4225f7e8ab28a6fc",
+    "url": "/channel/static/css/main.57652d0f.chunk.css"
+  },
+  {
+    "revision": "90209e40b5e7d095c493",
+    "url": "/channel/static/js/2.23418e27.chunk.js"
+  },
+  {
+    "revision": "ba46ae7961bea589e3a4a9ad90bea01e",
+    "url": "/channel/static/js/2.23418e27.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dccb4225f7e8ab28a6fc",
+    "url": "/channel/static/js/main.0813c76b.chunk.js"
+  },
+  {
+    "revision": "2569000dd9904e5f01bb",
+    "url": "/channel/static/js/runtime-main.a7c8c659.js"
+  },
+  {
+    "revision": "9189382bde56c6be9ec5addc8cedf96a",
+    "url": "/channel/static/media/add-feed.9189382b.png"
+  },
+  {
+    "revision": "eb2c40ee110adf8c67b8b8cf167235bb",
+    "url": "/channel/static/media/delete-feed.eb2c40ee.png"
+  }
+]);
