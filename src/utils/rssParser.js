521579212ad592e438c358dacf67e9801c12aab4
yarocruz <yarocruz@gmail.com>
2020-08-17 20:55:00 -0400

    made utility that adds hacker news as default feed

--- src/utils/rssParser.js
+++ src/utils/rssParser.js
@@ -1,14 +1,20 @@
 import Parser from "rss-parser";
+import {v4} from "uuid";
 
 const CORS_PROXY = `https://cors-anywhere.herokuapp.com/`;
-
 let parser = new Parser();
 
-const parseFeed = ((feedUrl) => {
-    parser.parseURL(`${CORS_PROXY}${feedUrl}`, (err, feed) => {
-        if (err) throw err;
-        console.log(feed.title);
-    })
-});
+const fetchDefaultFeed = async () => {
+    let defaultFeed = await parser.parseURL(`${CORS_PROXY}https://news.ycombinator.com/rss`);
+    return {
+        id: v4(),
+        feedRSS: 'https://news.ycombinator.com/rss',
+        feedTitle: defaultFeed.title,
+        feedDesc: defaultFeed.description,
+        feedUrl: defaultFeed.link,
+        feedItems: defaultFeed.items.length,
+        items: defaultFeed.items.map(obj => ({...obj, read: false})) // adding property to keep track of read items
+    };
+}
 
-export default parseFeed;
+export default fetchDefaultFeed;
