8e299244d21b7e7e5b705bfca71f6cf3bd08d06e
yarocruz <yarocruz@gmail.com>
2020-08-22 15:20:16 -0400

    giving it a facelift

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,10 +1,14 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import RssIcon from '../assets/rss-square.png';
 
 export default function Navbar() {
     return (
         <div className='navbar'>
-            <h1 className="logo"><Link to='/'>Channel</Link></h1>
+            <div className="logo">
+                <img className='rss-icon' src={RssIcon} alt="RSS icon"/>
+                <h1><Link to='/'>Channel</Link></h1>
+            </div>
             <nav>
                 <a href="https://en.wikipedia.org/wiki/RSS" target='_blank' rel='noopener noreferrer'>What is RSS?</a>
                 <Link to='/guide'>Guide</Link>
